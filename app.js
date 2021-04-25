67a791c5d73de42d704abdff71c769754b43816b
o0riggs0o <0Riggso@gmail.com>
2021-04-25 13:58:49 -0600

    changes

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 const express = require('express');
+const path = require('path')
 const app = express();
-const { products } = require('./data')
+
+// setup static and middlweware
+app.use(express.static('./dist/'))
 
 app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
-})
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return{id,name,image}
-  })
-
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const { productID } = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-      return res.status(404).send('Product Does Not Exist')
-  }
-
-  return res.json(singleProduct)
+  res.sendFile(path.resolve(__dirname,'./dist/index.html'))
+// adding to static assets
+// SSR
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
 })
 
 app.listen(5000, () => {
-  console.log('Server Is active on port 5000...');
+  console.log('server is listening on port 5000...')
 })
